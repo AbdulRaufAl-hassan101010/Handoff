702f21b2e795b2518ed986148462c92fac1dac3a
abdulkhaliqsoule <abdulkhaliqsoule@gmail.com>
2023-05-20 09:49:19 +0000

    sign up and in

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from models.forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,27 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
     return render_template("index.html")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
