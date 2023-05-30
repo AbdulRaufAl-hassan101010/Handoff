285a60419b87f9a76aa0ca9a79d5aeb2d93a6b8d
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-05-30 21:51:59 +0000

    created logo

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from handoffmls import app, db
 
 
 if __name__ == "__main__":
-    """UNCOMMENT WHEN RUNNING THE FIRST TIME"""
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", debug=True, port=5000)
-#else:
-    app.run()
-    
+    app.run(host="0.0.0.0", debug=True, port=5000)   
