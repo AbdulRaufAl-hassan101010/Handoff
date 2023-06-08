7b445cb7aba5d3d04f91c962bc376302aeac49e9
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-06-08 21:00:19 +0000

    done

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from handoffmls import app, db
-# imported tables to make db.reflect  create tables in order specified 
+from handoffmls import create_app, db
+# imported tables to make db.reflect  create tables in order specified
 from handoffmls.models.lab import Lab
 from handoffmls.models.user import User
 from handoffmls.models.task import Task
 from handoffmls.models.handoff import Handoff
+
+
+app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
         # Define the tables in the desired order
         db.reflect()
         db.create_all()
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5002)
--- handoffmls/__init__.py
+++ handoffmls/__init__.py
@@ -10,6 +10,8 @@ db_password = environ.get("db_password")
 db_name = environ.get("db_name")
 db_host = environ.get("db_host")
 
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ab95dd6cf1628f866d9b4e6f27b7ecce'
 # app.secret_key = environ.get('handoff_secret')
@@ -20,4 +22,7 @@ from handoffmls.landingpage import landingpage
 from handoffmls.dashboard import dashboard
 app.register_blueprint(landingpage, url_prefix="")
 app.register_blueprint(dashboard, url_prefix="/dashboard")
+    
 
+def create_app():
+    return app
